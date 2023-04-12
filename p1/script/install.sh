01cf74cdafd78b8d8a4a003fa03e1889a732483e
waxdred <waxcoin@hotmail.com>
2023-04-12 03:00:33 +0200

    is work

--- p1/script/install.sh
+++ p1/script/install.sh
@@ -1,32 +1,37 @@
 #!/bin/sh
 
-# Deploy keys to allow all nodes to connect each others as root
+echo [1] mv id_rsa
+mv /home/vagrant/tmp/id_rsa /home/vagrant/.ssh/id_rsa
 
-chmod 400 ~/.ssh/.id_rsa.pub
-chmod 400 ~/.ssh/d_rsa
-chown root:root  /home/vagrant/id_rsa.pub
-chown root:root  /home/vagrant/id_rsa
+echo [2] mv id_rsa.pub
+mv /home/vagrant/tmp/id_rsa.pub /home/vagrant/.ssh/id_rsa.pub
 
-cat /home/vagrant/id_rsa.pub >> /root/.ssh/authorized_keys
-chmod 400 /home/vagrant/authorized_keys
-chown root:root /home/vagrant/authorized_keys
+echo [3] get authorized_keys
+cat /home/vagrant/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
 
-# Add current node in  /etc/hosts
-echo "127.0.1.1 $(hostname)" >> /etc/hosts
+echo [4] chmod
+chmod 400 /home/vagrant/.ssh/id_rsa.pub
+chmod 400 /home/vagrant/.ssh/id_rsa
+chmod 400 /home/vagrant/.ssh/authorized_keys
 
-# Get current IP adress to launch k3S
 current_ip=$(/sbin/ip -o -4 addr list eth1 | awk '{print $4}' | cut -d/ -f1)
+echo [5] ip ${current_ip}
+echo [INFO] hostname $(hostname)
 
-# If we are on first node, launch k3s with cluster-init, else we join the existing cluster
 if [ $(hostname) = "jmilhasS" ]
     then
-        curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --bind-address=${current_ip} --flannel-iface=eth1 --write-kubeconfig-mode 644  --no-deploy=traefik" sh -
-else
-    echo "10.0.0.11  jmilhasK" >> /etc/hosts
-    scp -o StrictHostKeyChecking=no root@jmilhasS:/var/lib/rancher/k3s/server/token /tmp/token
-    curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --server https://jmilhasS:6443 --token-file /tmp/token --bind-address=${current_ip} --no-deploy=traefik" sh -
+        echo [6] install k3s
+        curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--bind-address=${current_ip} --flannel-iface=eth1 --write-kubeconfig-mode 644" sh -
+    else
+        echo [6] "get token  shh vagrant@192.168.56.110 | install k3s"
+        TOKEN=$(ssh -o StrictHostKeyChecking=no -i /home/vagrant/.ssh/id_rsa vagrant@192.168.56.110 "sudo cat /var/lib/rancher/k3s/server/node-token")
+
+        export K3S_TOKEN="$TOKEN"
+        export K3S_URL="https://192.168.56.110:6443"
+        echo "token ${K3S_TOKEN}"
+        export INSTALL_K3S_EXEC="agent --flannel-iface=eth1 "
+        curl -sfL https://get.k3s.io |  sh -
 fi
 
-# Wait for node to be ready and disable deployments on it
+echo [7] waiting k3s
 sleep 15
-kubectl get node -o wide

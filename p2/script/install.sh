2357f55b3168b60f9b884b31500bac862de47bc2
waxdred <waxcoin@hotmail.com>
2024-03-15 12:48:27 +0100

    update

--- p2/script/install.sh
+++ p2/script/install.sh
@@ -8,14 +8,14 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--bind-address=${current_ip} --
 
 sleep 42;
 
-echo [3] deploy services app1
-kubectl apply -f ./deploy_app.yml
+echo [3] deploy apps
+kubectl apply -f ./apps/.
 
-echo [4] deploy services app2
-kubectl apply -f ./deploy_app1.yml
+echo [4] deploy services
+kubectl apply -f ./Services/.
 
-echo [5] deploy services ingress
-kubectl apply -f ./deploy_ingress.yml
+echo [5] deploy ingress
+kubectl apply -f ./Ingress/.
 
 echo [6] waiting k3s
 kubectl wait --for=condition=Ready --timeout=600s pod --all 
